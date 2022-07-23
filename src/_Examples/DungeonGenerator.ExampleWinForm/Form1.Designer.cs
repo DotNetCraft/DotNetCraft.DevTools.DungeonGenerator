8d28e6d74deb891e0f61593dea585b459bfcdd09
DotNetCraft <dotnetcraft@gmail.com>
2022-07-23 16:45:38 +1000

    Added Winform example

---
+++ src/_Examples/DungeonGenerator.ExampleWinForm/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace DungeonGenerator.ExampleWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxScale = new System.Windows.Forms.TextBox();
+            this.textBoxMaxSize = new System.Windows.Forms.TextBox();
+            this.textBoxMinSize = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.Location = new System.Drawing.Point(23, 26);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(819, 646);
+            this.drawPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(860, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(860, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(982, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(860, 44);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 23);
+            this.textBoxWidth.TabIndex = 3;
+            this.textBoxWidth.Text = "40";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(982, 44);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHeight.TabIndex = 4;
+            this.textBoxHeight.Text = "40";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(860, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Scale";
+            // 
+            // textBoxScale
+            // 
+            this.textBoxScale.Location = new System.Drawing.Point(860, 92);
+            this.textBoxScale.Name = "textBoxScale";
+            this.textBoxScale.Size = new System.Drawing.Size(100, 23);
+            this.textBoxScale.TabIndex = 6;
+            this.textBoxScale.Text = "10";
+            // 
+            // textBoxMaxSize
+            // 
+            this.textBoxMaxSize.Location = new System.Drawing.Point(982, 141);
+            this.textBoxMaxSize.Name = "textBoxMaxSize";
+            this.textBoxMaxSize.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMaxSize.TabIndex = 10;
+            this.textBoxMaxSize.Text = "20";
+            // 
+            // textBoxMinSize
+            // 
+            this.textBoxMinSize.Location = new System.Drawing.Point(860, 141);
+            this.textBoxMinSize.Name = "textBoxMinSize";
+            this.textBoxMinSize.Size = new System.Drawing.Size(100, 23);
+            this.textBoxMinSize.TabIndex = 9;
+            this.textBoxMinSize.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(982, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "MaxSize";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(860, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "MinSize";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(860, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 39);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "MST";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 782);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBoxMaxSize);
+            this.Controls.Add(this.textBoxMinSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxScale);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.drawPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxScale;
+        private System.Windows.Forms.TextBox textBoxMaxSize;
+        private System.Windows.Forms.TextBox textBoxMinSize;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ src/_Examples/DungeonGenerator.ExampleWinForm/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetCraft.DevTools.DungeonGenerator.Core.BinarySpacePartitioning;
+using DotNetCraft.DevTools.DungeonGenerator.Core.Geometry;
+using DotNetCraft.DevTools.DungeonGenerator.Core.Geometry.Vectors;
+using DotNetCraft.DevTools.DungeonGenerator.Core.Graphs;
+
+namespace DungeonGenerator.ExampleWinForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly IBinarySpacePartitioningBuilder _binarySpacePartitioningBuilder;
+        private readonly IGraphBuilder _graphBuilder;
+        private readonly IGraphAlgorithms _graphAlgorithms;
+        private Graphics _drawPanelGraphics;
+
+        private Graph _graph;
+
+        public Form1(IBinarySpacePartitioningBuilder binarySpacePartitioningBuilder, IGraphBuilder graphBuilder, IGraphAlgorithms graphAlgorithms)
+        {
+            _binarySpacePartitioningBuilder = binarySpacePartitioningBuilder ?? throw new ArgumentNullException(nameof(binarySpacePartitioningBuilder));
+            _graphBuilder = graphBuilder ?? throw new ArgumentNullException(nameof(graphBuilder));
+            _graphAlgorithms = graphAlgorithms ?? throw new ArgumentNullException(nameof(graphAlgorithms));
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var scale = int.Parse(textBoxScale.Text);
+            var width = int.Parse(textBoxWidth.Text);
+            var height = int.Parse(textBoxHeight.Text);
+
+            var mainRect = new Rect(0, 0, width, height);
+            var buildConfig = new BuildConfig
+            {
+                MinSize = int.Parse(textBoxMinSize.Text),
+                MaxSize = int.Parse(textBoxMaxSize.Text)
+            };
+
+            var leaves = _binarySpacePartitioningBuilder.Build(mainRect, buildConfig);
+
+            _graph = _graphBuilder.Build(leaves);
+            var mstGraph = _graphAlgorithms.Kruskals_MST(_graph);
+            
+            _drawPanelGraphics.Clear(Color.White);
+            _drawPanelGraphics.DrawRectangle(new Pen(Color.Black), 0, 0, scale * width, scale * height);
+
+            var leafPen = new Pen(Color.Blue);
+            foreach (var leaf in leaves)
+            {
+                if (leaf.ActiveLeaf)
+                    _drawPanelGraphics.DrawRectangle(leafPen, scale * leaf.Bounds.X, scale * leaf.Bounds.Y, scale * leaf.Bounds.Width, scale * leaf.Bounds.Height);
+            }
+
+            var centers = leaves.Where(x => x.ActiveLeaf).Select(x => x.Bounds.Center * scale).ToList();
+            
+            var edgePen = new Pen(Color.Red);
+            var vertexPen = new Pen(Color.Red);
+            DrawGraph(_graph, centers, vertexPen, scale, edgePen);
+
+            edgePen = new Pen(Color.Green, 2);
+            vertexPen = new Pen(Color.Green);
+            DrawGraph(mstGraph, centers, vertexPen, scale, edgePen);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawGraph(Graph graph, List<Vector2> centers, Pen vertexPen, int scale, Pen edgePen)
+        {
+            foreach (var vertex in graph.Vertices)
+            {
+                var edges = graph.Edges.Where(x => x.Vertex1 == vertex);
+                foreach (var edge in edges)
+                {
+                    var c1 = centers[edge.Vertex1];
+                    var c2 = centers[edge.Vertex2];
+
+                    _drawPanelGraphics.DrawEllipse(vertexPen, (int)(c1.X - scale / 2.0), (int)(c1.Y - scale / 2.0), scale,
+                        scale);
+                    _drawPanelGraphics.DrawEllipse(vertexPen, (int)(c2.X - scale / 2.0), (int)(c2.Y - scale / 2.0), scale,
+                        scale);
+
+                    _drawPanelGraphics.DrawLine(edgePen, c1.X, c1.Y, c2.X, c2.Y);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _drawPanelGraphics = drawPanel.CreateGraphics();
+        }
+    }
+}
